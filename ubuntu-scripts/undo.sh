aa00a44ea0dcb808ab718fd117b517ab9025860a
RandomGuy <codymoores@live.com>
2023-03-26 13:12:27 -0230

    Updated README

--- ubuntu-scripts/undo.sh
+++ ubuntu-scripts/undo.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
 # Remove Caffeine
-sudo apt purge --auto-remove caffeine
+sudo apt purge --auto-remove caffeine -y
 
 
 # Remove Wine and its dependencies
-sudo apt purge --auto-remove winehq-stable
+sudo apt purge --auto-remove winehq-stable -y
 
 # Remove WineHQ key and sources file
-sudo rm /etc/apt/keyrings/winehq-archive.key
-sudo rm /etc/apt/sources.list.d/winehq-focal.sources
+sudo rm /etc/apt/keyrings/winehq-archive.key -y
+sudo rm /etc/apt/sources.list.d/winehq-focal.sources -y
 
 # Remove Docker, containerd, and Docker Compose
-sudo apt purge --auto-remove docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt purge --auto-remove docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 # Remove Docker key and sources file
-sudo rm /etc/apt/keyrings/docker.gpg
-sudo rm /etc/apt/sources.list.d/docker.list
+sudo rm /etc/apt/keyrings/docker.gpg -y
+sudo rm /etc/apt/sources.list.d/docker.list -y
 
 # Remove Docker group for the current user
-sudo gpasswd -d $USER docker
+sudo gpasswd -d $USER docker -y
 
 # Remove BYOB prerequisites
 sudo pip3 uninstall -y Flask-SQLAlchemy swig pyinstaller
 
 # Remove other packages
-sudo apt purge --auto-remove git python3-pip
+sudo apt purge --auto-remove git python3-pip -y
 
 # Remove BYOB repository
-rm -rf ~/byob
+rm -rf ~/byob -y
 
 # Update and upgrade the system
 sudo apt update && sudo apt upgrade -y
